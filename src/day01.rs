449d6deb141047348d30c6e4c9e1cd5358900754
michele.bianchi@embark-studios.com <michele.bianchi@embark-studios.com>
2021-12-03 16:26:12 +0100

    Completed Day01-Part1

--- src/day01.rs
+++ src/day01.rs
@@ -1,3 +1,24 @@
 pub fn part1(input: String) {
-    println!("{}", input.chars().rev().collect::<String>());
+    let mut line_input = input.split('\n');
+
+    let mut increase_amount = 0;
+    let mut last_value: u32 = line_input.next().expect("An empty input, really?").trim().parse::<u32>().expect("The first line is not an integer!");
+
+    for curr_value in line_input {
+        let num_opt = curr_value.trim().parse::<u32>();
+        let num_value = match num_opt {
+            Ok(val) => {val}
+            Err(e) => {
+                println!("String {} doesn't seem to contain a number! Error: {}", curr_value, e);
+                last_value
+            }
+        };
+
+        if num_value > last_value {
+            increase_amount += 1;
+        }
+        last_value = num_value;
+    }
+
+    println!("Amount of increases: {}", increase_amount);
 }
