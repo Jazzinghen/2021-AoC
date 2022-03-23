6b0ef44220edc412be76624b3a01e3919a5044af
michele.bianchi@embark-studios.com <michele.bianchi@embark-studios.com>
2022-03-23 13:52:05 +0100

    FINALLY COMPLETED DAY 17!

--- src/days/day17.rs
+++ src/days/day17.rs
@@ -1,5 +1,8 @@
 use std::cmp::{max, min};
+use std::collections::HashSet;
+use std::iter::FromIterator;
 
+use itertools::Itertools;
 use nom::bytes::complete::tag;
 use nom::sequence::{preceded, separated_pair};
 use nom::IResult;
@@ -37,6 +40,93 @@ impl Trench {
     pub fn coolest_speed(&self) -> Point {
         (self.best_x(), -(self.bottom_right.1 + 1i32))
     }
+
+    fn get_potential_y_velocities(&self) -> Vec<(i32, i32)> {
+        let max_y = -(self.bottom_right.1 + 1i32);
+        let mut available_velocities: Vec<(i32, i32)> = Vec::new();
+        for start_y in 0i32..=max_y {
+            let mut y_check = 0i32;
+            let mut step = 0i32;
+            while y_check > self.top_left.1 {
+                y_check -= start_y + step;
+                step += 1i32;
+            }
+
+            while y_check >= self.bottom_right.1 {
+                available_velocities.push((-start_y, step - 1i32));
+                y_check -= start_y + step;
+                step += 1i32;
+            }
+        }
+
+        let mut extra_velocities: Vec<(i32, i32)> = Vec::new();
+        for (vel, steps) in available_velocities.iter() {
+            if *vel < 0i32 {
+                extra_velocities.push((-*vel, -*vel * 2 + steps));
+            }
+        }
+
+        available_velocities.extend(extra_velocities);
+        available_velocities.push((self.bottom_right.1, 0i32));
+        available_velocities
+    }
+
+    fn get_all_zero_x(&self) -> Vec<i32> {
+        let mut curr_x_speed = self.best_x();
+        let mut zero_x_velocities: Vec<i32> = Vec::new();
+        while curr_x_speed * (curr_x_speed + 1i32) / 2i32 <= self.bottom_right.0 {
+            zero_x_velocities.push(curr_x_speed);
+            curr_x_speed += 1i32;
+        }
+
+        zero_x_velocities
+    }
+
+    fn get_x_velocities(&self, steps: i32) -> Vec<i32> {
+        let mut x_vels: Vec<i32> = Vec::new();
+
+        for pot_vel in steps..=self.bottom_right.0 {
+            let final_x_vel = pot_vel - steps;
+            let final_x =
+                ((pot_vel + 1i32) * pot_vel) / 2i32 - ((final_x_vel + 1i32) * final_x_vel) / 2i32;
+
+            if final_x >= self.top_left.0 && final_x <= self.bottom_right.0 {
+                x_vels.push(pot_vel);
+            }
+        }
+
+        x_vels
+    }
+
+    fn one_step_velocities(&self) -> Vec<Point> {
+        let platform_locations = (self.top_left.0..=self.bottom_right.0)
+            .cartesian_product(self.bottom_right.1..=self.top_left.1);
+
+        platform_locations.collect_vec()
+    }
+
+    pub fn compute_initial_velocities(&self) -> HashSet<Point> {
+        let mut velocities: HashSet<Point> =
+            HashSet::from_iter(self.one_step_velocities().into_iter());
+
+        let all_initial_y_vels = self.get_potential_y_velocities();
+
+        for (vel, steps) in all_initial_y_vels.iter() {
+            let x_vels = self.get_x_velocities(*steps + 1);
+            let vel_combos = x_vels.into_iter().map(|x| (x, *vel));
+            velocities.extend(vel_combos);
+        }
+
+        let zero_x_vels = self.get_all_zero_x();
+
+        for x in zero_x_vels {
+            for (y, _) in all_initial_y_vels.iter().filter(|(_, steps)| *steps >= x) {
+                velocities.insert((x, *y));
+            }
+        }
+
+        velocities
+    }
 }
 
 // Edge parser
@@ -66,27 +156,34 @@ pub fn part1(input: &str) {
     println!("Maximum height for provided trench: {}", max_height);
 }
 
-// pub fn part2(input: &str) {
-//     let cave_net = CaveNetwork::new(input);
-//     let path_count = cave_net.find_paths(true);
-//     println!(
-//         "Amount of unique paths to the exit considering repeating caves: {}",
-//         path_count
-//     );
-// }
+pub fn part2(input: &str) {
+    let (_, target_trench) = target(input).unwrap();
+    let initial_velocities: HashSet<Point> = target_trench.compute_initial_velocities();
+    println!("Amount of initial velocities: {}", initial_velocities.len());
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    use std::collections::HashSet;
-    use std::iter::FromIterator;
+    use nom::character::complete::multispace1;
+    use nom::multi::separated_list1;
 
     impl Trench {
         pub fn is_point_inside(&self, point: &Point) -> bool {
             (point.0 >= self.top_left.0 && point.0 <= self.bottom_right.0)
                 && (point.1 <= self.top_left.1 && point.1 >= self.bottom_right.1)
         }
+    }
+
+    fn vel(input: &str) -> IResult<&str, Point> {
+        let (remain_str, (vel_x, vel_y)) =
+            separated_pair(jazz_parser::i32, tag(","), jazz_parser::i32)(input)?;
+        Ok((remain_str, (vel_x, vel_y)))
+    }
+
+    fn velocities(input: &str) -> IResult<&str, Vec<Point>> {
+        separated_list1(multispace1, vel)(input)
     }
 
     fn compute_trajectory(initial_velocities: &Point, target: &Trench) -> Vec<Point> {
@@ -198,5 +295,52 @@ mod tests {
         let max_height: i32 = start_v.1 * (start_v.1 + 1i32) / 2i32;
 
         assert_eq!(max_height, 45i32);
+    }
+
+    #[test]
+    fn compute_available_y() {
+        let input_string = "target area: x=20..30, y=-10..-5";
+        let (_, target_trench) = target(input_string).unwrap();
+
+        let ref_y_velocities: HashSet<i32> = HashSet::from_iter(vec![
+            -8, 4, -5, -3, -9, -7, 6, 9, -4, 5, 7, -10, 0, 2, 8, -2, -1, 1, -6, 3,
+        ]);
+
+        let computed_y_vels: HashSet<i32> = HashSet::from_iter(
+            target_trench
+                .get_potential_y_velocities()
+                .into_iter()
+                .map(|(y, _)| y),
+        );
+
+        assert_eq!(computed_y_vels, ref_y_velocities);
+    }
+
+    #[test]
+    fn compute_all_start_velocities() {
+        let input_string = "target area: x=20..30, y=-10..-5";
+        let (_, target_trench) = target(input_string).unwrap();
+
+        let input_result = "23,-10  25,-9   27,-5   29,-6   22,-6   21,-7   9,0     27,-7   24,-5
+            25,-7   26,-6   25,-5   6,8     11,-2   20,-5   29,-10  6,3     28,-7
+            8,0     30,-6   29,-8   20,-10  6,7     6,4     6,1     14,-4   21,-6
+            26,-10  7,-1    7,7     8,-1    21,-9   6,2     20,-7   30,-10  14,-3
+            20,-8   13,-2   7,3     28,-8   29,-9   15,-3   22,-5   26,-8   25,-8
+            25,-6   15,-4   9,-2    15,-2   12,-2   28,-9   12,-3   24,-6   23,-7
+            25,-10  7,8     11,-3   26,-7   7,1     23,-9   6,0     22,-10  27,-6
+            8,1     22,-8   13,-4   7,6     28,-6   11,-4   12,-4   26,-9   7,4
+            24,-10  23,-8   30,-8   7,0     9,-1    10,-1   26,-5   22,-9   6,5
+            7,5     23,-6   28,-10  10,-2   11,-1   20,-9   14,-2   29,-7   13,-3
+            23,-5   24,-8   27,-9   30,-7   28,-5   21,-10  7,9     6,6     21,-5
+            27,-10  7,2     30,-9   21,-8   22,-7   24,-9   20,-6   6,9     29,-5
+            8,-2    27,-8   30,-5   24,-7";
+
+        let (_, reference_velocities) = velocities(input_result).unwrap();
+
+        let reference_set: HashSet<Point> = HashSet::from_iter(reference_velocities.into_iter());
+
+        let initial_velocities: HashSet<Point> = target_trench.compute_initial_velocities();
+
+        assert_eq!(initial_velocities, reference_set);
     }
 }
